158e8ef8d8bafc21a589a3bfff0540b935a17c98
NikaelaBalla <76441747+NikaelaBalla@users.noreply.github.com>
2021-01-14 09:53:53 +0100

    Add files via upload

---
+++ AdminandUserProject/CreateUser.php
@@ -0,0 +1,137 @@
+<?php include('connect.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create user</title>
+	<style>
+	* { margin: 0px; padding: 0px; }
+body {
+	font-size: 100%;
+	background-image: url('CreateUserBC.jpg');
+	background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: 100% 100%;
+}
+.header {
+	width: 50%;
+	margin: 50px auto 0px;
+	color: Black;
+	background: :orange;
+	text-align: center;
+	border: 1px solid #d1cb71;
+	border-bottom: none;
+	border-radius: 10px 10px 0px 0px;
+	padding: 20px;
+}
+form, .content {
+	width: 50%;
+	margin: 0px auto;
+	padding: 20px;
+	border: 1px solid ;
+	background: white;
+	border-radius: 0px 0px 10px 10px;
+}
+.input-group {
+	margin: 10px 0px 10px 0px;
+}
+.input-group label {
+	display: block;
+	text-align: left;
+	margin: 3px;
+}
+.input-group input {
+	height: 30px;
+	width: 93%;
+	padding: 5px 10px;
+	font-size: 16px;
+	border-radius: 5px;
+	border: 1px solid gray;
+}
+#user_type {
+	height: 40px;
+	width: 98%;
+	padding: 5px 10px;
+	background: white;
+	font-size: 16px;
+	border-radius: 5px;
+	border: 1px solid gray;
+}
+.btn {
+	padding: 10px;
+	font-size: 15px;
+	color: white;
+	background: pink;
+	border: none;
+	border-radius: 5px;
+}
+.error {
+	width: 92%; 
+	margin: 0px auto; 
+	padding: 10px; 
+	border: 1px solid #a94442; 
+	color: #a94442; 
+	background: #f2dede; 
+	border-radius: 5px; 
+	text-align: left;
+}
+.success {
+	color: #3c763d; 
+	background: #dff0d8; 
+	border: 1px solid #3c763d;
+	margin-bottom: 20px;
+}
+.profile_info img {
+	display: inline-block; 
+	width: 50px; 
+	height: 50px; 
+	margin: 5px;
+	float: left;
+}
+.profile_info div {
+	display: inline-block; 
+	margin: 5px;
+}
+.profile_info:after {
+	content: "";
+	display: block;
+	clear: both;
+}
+		
+	</style>
+</head>
+<body>
+	<div class="header">
+		<h2>Create user</h2>
+	</div>
+	
+	<form method="post" action="CreateUser.php">
+
+		<?php echo display_error(); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" value="<?php echo $username; ?>">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>User type</label>
+			<select name="user_type" id="user_type" >
+				<option value=""></option>
+				<option value="admin">Admin</option>
+				<option value="user">User</option>
+			</select>
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		
+		<div class="input-group">
+			<button type="submit" class="btn" name="register_btn"> + Create user</button>
+		</div>
+	</form>
+</body>
+</html>
